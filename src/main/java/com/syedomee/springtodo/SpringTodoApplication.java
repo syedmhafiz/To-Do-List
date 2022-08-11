885f3cc14c4fdf386fe8a591b2e5f93bea92c858
syedomee <syedhafiz@gmail.com>
2022-08-12 02:49:30 +0600

    Second Commit

--- src/main/java/com/syedomee/springtodo/SpringTodoApplication.java
+++ src/main/java/com/syedomee/springtodo/SpringTodoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringTodoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		SpringApplication.run(SpringTodoApplication.class, args);
 	}
 
